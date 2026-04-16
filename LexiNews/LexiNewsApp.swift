23f3dd394db5c1e51a3bd507fec2c1b6f83681a3
Ali Altinok <info.alialtinok@gmail.com>
2026-04-16 18:02:13 -0400

    Initial Commit

---
+++ LexiNews/LexiNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LexiNewsApp.swift
+//  LexiNews
+//
+//  Created by Ali Altinok on 2026-04-16.
+//
+
+import SwiftUI
+
+@main
+struct LexiNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
